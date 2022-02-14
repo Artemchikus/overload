2be863bee0948eaed0dc1772825f0f4da7b9e708
Artemiks <fil.ar.alex@gmail.com>
2022-02-14 18:39:00 +0400

    reset + docker

--- testserver/testserver.go
+++ testserver/testserver.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 )
 
 var requestNumber int
 
 func main() {
 	http.HandleFunc("/test", handleTest())
+	http.HandleFunc("/reset", handleReset())
 	log.Printf("Starting API server on port %s", ":8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func handleTest() http.HandlerFunc{
+func handleTest() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Getting request number: %v", requestNumber)
 		requestNumber++
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func handleReset() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Reseting number of requests")
+		requestNumber = 0
 		w.WriteHeader(http.StatusOK)
 	}
 }
